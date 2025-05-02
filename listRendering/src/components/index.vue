f8cb74adc48dfe3b05fd89980eeb56c85c716b10
HendiStudent <hendisyaputra@student.uir.ac.id>
2025-05-02 16:10:24 +0700

    menambah_style

--- listRendering/src/components/index.vue
+++ listRendering/src/components/index.vue
@@ -1,28 +1,53 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
+let id = 0
+const h1 = ref('Hello World')
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'Belajar DataBase', done: true },
+  { id: id++, text: 'Belajar Pemrograman', done: true },
+  { id: id++, text: 'Belajar VueJs', done: false }
+])
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter((t) => !t.done)
+    : todos.value
+})
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
-
+<h1 ></h1>
 <template>
-<div class="container">
-  <h1 class="mt-3 text-center">Todo List </h1>
-</div>
-<div class="col-9 ">
-  <div class="col-2">
-    <input type="text" class="form-control" placeholder="Start Time">
-  </div>
-  <div class="col-2">
-    <input type="text" class="form-control" placeholder="Finis Time">
-  </div>
-  <div class="col-6">
-    <input type="text" class="form-control" placeholder="New Activity">
-  </div>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
   
+  <ul>
+  <li v-for="todo in filteredTodos" :key="todo.id">
+    <input type="checkbox" v-model="todo.done">
+    <span :class="{ done: todo.done }">{{ todo.text }}</span>
+    <button @click="removeTodo(todo)">🗑</button>
+  </li>
+</ul>
   
-</div>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
 
-<style scoped>
-
-
+<style>
+.done {
+  text-decoration: line-through;
+}
 </style>
