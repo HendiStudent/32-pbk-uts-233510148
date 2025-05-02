53ccd6a0d718fa64ec4968a11a77627c4c789412
HendiStudent <hendisyaputra@student.uir.ac.id>
2025-05-02 11:31:35 +0700

    second_commit

---
+++ listRendering/src/components/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+<div class="container">
+  <h1 class="mt-3 text-center">Todo List </h1>
+</div>
+<div class="col-9 ">
+  <div class="col-2">
+    <input type="text" class="form-control" placeholder="Start Time">
+  </div>
+  <div class="col-2">
+    <input type="text" class="form-control" placeholder="Finis Time">
+  </div>
+  <div class="col-6">
+    <input type="text" class="form-control" placeholder="New Activity">
+  </div>
+  
+  
+</div>
+</template>
+
+<style scoped>
+
+
+</style>
